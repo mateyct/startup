23e75be207bb66e0d1dca63dcf6e459e5afacc0d
mateyct <masonctolley1@gmail.com>
2025-01-12 00:45:24 -0700

    now the whole board is in a 2D array

--- clue/game.js
+++ clue/game.js
@@ -8,8 +8,8 @@ let board = document.getElementById("board");
 fetch("./dataFiles/rooms.json")
     .then((response) => response.json())
     .then((json) => {
-        let boardArr = [];
-        
+        // make a new array filled with 0s. 0s make it easier to fill with blanks later
+        let boardArr = new Array(24).fill().map(() => new Array(24).fill(0));
         json.rooms.forEach(element => {
             let room = document.createElement("div");
             if ("void" in element) {
@@ -18,31 +18,40 @@ fetch("./dataFiles/rooms.json")
             else {
                 room.classList.add(element.name, "room");
                 room.textContent = element.displayName;
-                // also make it have correct styling
-                room.style.gridColumn = element.posX + "/" + (element.posX + 6);
-                room.style.gridRow = element.posY + "/" + (element.posY + 6);
-                if ("doors" in element) {
-                    element.doors.forEach(doorData => {
-                        let door = document.createElement("div");
-                        door.className = "door";
-                        let xPos = element.posX + doorData.x;
-                        let yPos = element.posY + doorData.y;
-                        door.style.gridColumn = xPos + " / " + (xPos + 1);
-                        door.style.gridRow = yPos + " / " + (yPos + 1);
-                        board.appendChild(door);
-                    });
+            }
+            // also make it have correct styling
+            room.style.gridColumn = element.posX + "/" + (element.posX + 6);
+            room.style.gridRow = element.posY + "/" + (element.posY + 6);
+            // loop to fill up my nice array! (boardArr)
+            for (let i = element.posX - 1; i < element.posX + 5; i++) {
+                for (let j = element.posY - 1; j < element.posY + 5; j ++) {
+                    boardArr[j][i] = room;
                 }
+            }
+            if ("doors" in element) {
+                element.doors.forEach(doorData => {
+                    let door = document.createElement("div");
+                    door.className = "door";
+                    let xPos = element.posX + doorData.x;
+                    let yPos = element.posY + doorData.y;
+                    door.style.gridColumn = xPos + " / " + (xPos + 1);
+                    door.style.gridRow = yPos + " / " + (yPos + 1);
+                    boardArr[yPos - 1][xPos - 1] = door;
+                    board.appendChild(door);
+                });
             }
             board.appendChild(room);
         });
         // calculate remaining space
-        let totalSpace = 24 * 24;
-        let roomSpace = 6 * 6;
-        let takenSpace = roomSpace * 9;
-        let emptySpace = totalSpace - takenSpace;
-        for (let i = 0; i < emptySpace; i++) {
-            let newCell = document.createElement("div");
-            newCell.classList.add("hall");
-            board.appendChild(newCell);
+        for (let i = 0; i < boardArr.length; i++) {
+            for (let j = 0; j < boardArr[i].length; j++) {
+                if (boardArr[i][j] == 0) {
+                    let newCell = document.createElement("div");
+                    newCell.classList.add("hall");
+                    boardArr[i][j] = newCell;
+                    board.appendChild(newCell);
+                }
+            }
         }
+        console.log(boardArr);
     });
