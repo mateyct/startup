a62296b560acb732a18eeb85df021ae6be1ff35d
mateyct <masonctolley1@gmail.com>
2025-01-30 12:42:46 -0700

    play page should be done

--- js/modals.js
+++ js/modals.js
@@ -1,13 +1,19 @@
 "use strict";
 window.onload = () => {
     let controlPanel = document.querySelector('#controls');
-    let chatPanel = document.querySelector('.chatbox');
+    let chatPanel = document.querySelector('#chat-box');
     // add open modal code
     document.getElementById('open-controls').addEventListener('click', () => {
         controlPanel.classList.add('modal');
     });
+    document.getElementById('open-chat').addEventListener('click', () => {
+        chatPanel.classList.add('modal');
+    });
     // add close modal code
     document.getElementById("close-controls").addEventListener("click", () => {
         controlPanel.classList.remove('modal');
+    });
+    document.getElementById('close-chat').addEventListener('click', () => {
+        chatPanel.classList.remove('modal');
     });
 };
