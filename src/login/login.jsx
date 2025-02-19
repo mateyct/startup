8eec7e176f8da84a385aa14f3a944401f3b03583
mateyct <masonctolley1@gmail.com>
2025-02-18 22:14:30 -0700

    conditional authentication should be immplemented

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,23 +5,44 @@ import { Auth } from "./auth";
 export function Login(props) {
     const [userName, setUserName] = useState(props.userName);
     const [password, setPass] = useState('');
+    // function for logging in
+    function logIn() {
+        localStorage.setItem('userName', userName);
+        props.setUserName(userName);
+        props.setUserAuth(Auth.Authenticated);
+    }
+    // function for creating account
+    function create() {
+        localStorage.setItem('userName', userName);
+        props.setUserName(userName);
+        props.setUserAuth(Auth.Authenticated);
+    }
     return (
+
         <main className="index-main">
-            <form className="login-form" method="dialog">
-                <h2>Log in to Medical Murdery Mystery</h2>
-                <div>
+            {props.userAuth === Auth.Unauthenticated && (
+                <form className="login-form" method="dialog">
+                    <h2>Log in to Medical Murdery Mystery</h2>
                     <div>
-                        <label>Username</label>
-                        <input required type="text" id="username" name="username" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
+                        <div>
+                            <label>Username</label>
+                            <input required type="text" id="username" name="username" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input required type="password" id="password" name="password" placeholder="Password" onChange={e => setPass(e.target.value)} />
+                        </div>
+                        <input type="submit" name="login" value="Log In" className="my-button" disabled={!userName || !password} onClick={logIn} />
+                        <input type="submit" name="create" value="Create" className="my-button" disabled={!userName || !password} onClick={create} />
                     </div>
-                    <div>
-                        <label>Password</label>
-                        <input required type="password" id="password" name="password" placeholder="Password" onChange={e => setPass(e.target.value)} />
-                    </div>
-                    <input type="submit" name="login" value="Log In" className="my-button" disabled={!userName || !password} />
-                    <input type="submit" name="create" value="Create" className="my-button" disabled={!userName || !password}  />
-                </div>
-            </form>
+                </form>
+            )}
+            {props.userAuth === Auth.Authenticated && (
+                <button className="my-button" type="button" style={{borderRadius: 5}} onClick={() => {
+                    localStorage.removeItem('userName');
+                    props.setUserAuth(Auth.Unauthenticated);
+                }}>Log Out</button>
+            )}
         </main>
     );
 }
